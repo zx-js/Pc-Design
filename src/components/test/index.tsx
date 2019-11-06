9af0e88b3ff62bbcaccfb884d2a825fca7a190c1
wangxiaolong <xiaolong1498@gmail.com>
2019-11-06 14:45:22 +0800

    修改msg实例文字

--- src/components/test/index.tsx
+++ src/components/test/index.tsx
@@ -2,7 +2,7 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class MtTest extends Vue {
-    public msg: string = 'This is test page';
+    public msg: string = 'This is mt test page';
     public render(): any {
         return <h1>{this.msg}</h1>;
     }
