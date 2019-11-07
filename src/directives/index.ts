30ae17ebb25370d72a72b65361a3fc8790611bbe
wangxiaolong <xiaolong1498@gmail.com>
2019-11-07 16:22:00 +0800

    优化自动引入自定义指令重复引用的警告

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,11 +1,8 @@
 export default {
     install: (Vue: any) => {
-        const requireComponent = require.context('./', true, /\.(ts)/);
+        const requireComponent = require.context('./', true, /[^index]\.ts/);
         requireComponent.keys().map((filePath) => {
-            const fileName = filePath.slice(filePath.lastIndexOf('/') + 1).replace(/\..*$/, '');
-            if (fileName !== 'index') {
-                requireComponent(filePath)(Vue);
-            }
+            requireComponent(filePath)(Vue);
         });
     },
 };
