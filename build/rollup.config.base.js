d4e78c8ad09c13767c4ca3f0927611d130e0d4cf
bhabgs <bhabgs@aliyun.com>
2019-11-08 16:09:21 +0800

    xf

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -4,7 +4,7 @@
  * @Author: bhabgs
  * @Date: 2019-10-29 10:56:25
  * @LastEditors: bhabgs
- * @LastEditTime: 2019-11-08 09:39:55
+ * @LastEditTime: 2019-11-08 14:32:12
  */
 import resolve from 'rollup-plugin-node-resolve'; // 告诉 Rollup 如何查找外部模块
 import commonjs from 'rollup-plugin-commonjs'; // 将CommonJS模块转换为 ES2015 供 Rollup 处理
@@ -17,9 +17,6 @@ import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import replace from 'rollup-plugin-replace';
 
-const colors = {
-    blue: '#056ef0'
-}
 export default {
     input: 'src/index.ts',
     plugins: [
