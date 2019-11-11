75d14b34345d9c4202288488df1c05c0ede12d01
langxue <641078926@qq.com>
2019-11-11 14:41:24 +0800

    tag组件

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,10 +1,68 @@
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component
-export default class ZTag extends Vue {
+export default class Ztag extends Vue {
+    // tag大小
+    @Prop({
+        default: "large",
+        type: String,
+    }) public size!: string;
+    // tag是否显示
+    @Prop({
+        default: true,
+        type: Boolean,
+    }) public isVisible?: boolean;
+    // 是否显示删除按钮
+    @Prop({
+        default: true,
+        type: Boolean,
+    }) public isClose?: boolean;
+    // 是否有边框
+    @Prop({
+        default: true,
+        type: Boolean,
+    }) public isBorder?: boolean;
+    // 预设颜色
+    @Prop({
+        default: "",
+        type: String,
+    }) public color?: string;
+    // 设置颜色类
+    public setColorClass() {
+        return `z-tag-${this.color}`
+    }
+    // 设置大小
+    public setClass() {
+        return `z-tag-${this.size}`;
+    }
+    // 设置显示或隐藏
+    public setVisible() {
+        return this.isVisible;
+    }
+    // 设置是否删除
+    public setClose() {
+        return this.isClose;
+    }
+    // 设置边框
+    public setBorder() {
+        const border = this.isBorder ? 'z-tag-border' : '';
+        return border;
+    }
+    public renderIcon(): any {
+        return this.setClose() ? (
+            <i class="iconfont zxclose">X</i>
+        ) : null;
+    }
     public render(): any {
         return (
-            <span>Tag</span>
+            <button class={['z-tag', 
+                this.setClass(), 
+                this.setColorClass(),
+                this.setBorder()]}
+                v-show={this.setVisible()}>
+                tag666666
+                {this.renderIcon()}
+            </button>
         );
     }
 }
--- vuepress/vue/Tag/README.md
+++ vuepress/vue/Tag/README.md
@@ -12,7 +12,29 @@
 ### Who（lx）
 
 1. tag颜色定制
-2. tag 大小 size、big、mini
+2. tag 大小 size、large、small、mini  默认：large
 3. 可删除tag
 4. 删除是否需要确认
 5. ...自行发挥
+6. 是否边框
+7. 按钮位置：右上 右侧
+
+
+<template>
+  <Ztag :isBorder="true"
+    :isClose="true"
+    size="large"
+    color="pink"
+    v-for="(i, key) in 10"
+    :key="key"
+    @click="onClose" />
+</template>
+<script>
+  export default {
+    public onClose() {
+        console.log(1);
+      }
+  }
+</script>
+
+
