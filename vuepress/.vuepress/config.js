d6734ad8575b885d063838b91a0c60ec31808118
bhabgs <bhabgs@aliyun.com>
2019-11-12 16:35:39 +0800

    添加基础路径

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -4,12 +4,13 @@
  * @Author: bhabgs
  * @Date: 2019-11-06 16:39:45
  * @LastEditors: bhabgs
- * @LastEditTime: 2019-11-12 15:07:31
+ * @LastEditTime: 2019-11-12 16:08:04
  */
 const themeConfig = require('./config/themeConfig').themeConfig;
 module.exports = {
     title: 'zx-util-ui',
     port: 9991,
+    base: '/docs/',
     markdown: {
         plugins: [
         '@org/foo', // 等价于 @org/markdown-it-foo，如果对应的包存在
