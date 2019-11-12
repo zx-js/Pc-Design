ccc0d4d75ebc8b4c56d529d7f5c697f975b22e67
wangxiaolong <xiaolong1498@gmail.com>
2019-11-12 12:08:03 +0800

    nothing

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -9,9 +9,11 @@ export default class ZTimeline extends Vue {
 
     render() {
         let slots = this.$slots.default || [];
+
         if (this.reverse) {
             slots = slots.reverse();
         }
+
         return (
             <div class="z-timeline">
                 <div class="z-timeline-inner">{slots}</div>
