b0e5f86a4a3903f77ca9f6c290c0002a6af57aad
bhabgs <bhabgs@aliyun.com>
2019-11-14 16:09:58 +0800

    加入script脚本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,14 +3,14 @@
  * @version: 
  * @Author: bhabgs
  * @Date: 2019-11-06 16:39:45
- * @LastEditors: 王晓龙
- * @LastEditTime: 2019-11-12 16:58:17
+ * @LastEditors: bhabgs
+ * @LastEditTime: 2019-11-14 15:59:16
  */
 const themeConfig = require('./config/themeConfig').themeConfig;
 module.exports = {
     title: 'zx-util-ui',
     port: 9991,
-    base: '/docs/',
+    base: '/zx-util-ui-docs/',
     markdown: {
         plugins: [
         '@org/foo', // 等价于 @org/markdown-it-foo，如果对应的包存在
