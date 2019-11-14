5bba77e03b7789fb1f327713e67098b2e0a9e63f
langxue <641078926@qq.com>
2019-11-14 17:12:46 +0800

    input组件-禁用状态示例组件添加

---
+++ docs/.vuepress/components/Input/demo-disabled.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 
+ * @Date: 2019-11-14 16:03:05
+ * @LastEditors: langxue
+ * @LastEditTime: 2019-11-14 17:07:58
+ -->
+<template>
+  <div id="input-demo-disabled">
+    <z-input type="text"
+          placeholder="请输入内容"
+          disabled></z-input>
+  </div>
+</template>
+<style lang="stylus">
+#input-demo-disabled {
+  width 180px;
+}
+</style>
