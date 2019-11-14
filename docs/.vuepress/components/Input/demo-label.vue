fcf142b5c1444be18dc5a41ad279be1fca98ccb6
langxue <641078926@qq.com>
2019-11-14 17:13:28 +0800

    input组件 -  标签示例组件添加

---
+++ docs/.vuepress/components/Input/demo-label.vue
@@ -0,0 +1,21 @@
+<!--
+ * @abstract: 
+ * @version: 
+ * @Author: langxue
+ * @Date: 2019-11-12 11:07:52
+ * @LastEditors: langxue
+ * @LastEditTime: 2019-11-14 17:02:11
+ -->
+<template>
+  <div id="input-demo-label">
+    <z-input type="text"
+          label="label"
+          label-width="80px">label</z-input>
+  </div>
+</template>
+<style lang="stylus">
+#input-demo-label {
+  width 230px;
+  display flex;
+}
+</style>
