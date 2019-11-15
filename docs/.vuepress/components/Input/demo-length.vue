423676ad9b2a2543e897cc47c18b157447d12772
langxue <641078926@qq.com>
2019-11-15 15:35:28 +0800

    input组件 - 输入限制示例

--- docs/.vuepress/components/Input/demo-length.vue
+++ docs/.vuepress/components/Input/demo-length.vue
@@ -4,13 +4,12 @@
  * @Author: 
  * @Date: 2019-11-14 16:03:05
  * @LastEditors: langxue
- * @LastEditTime: 2019-11-14 17:07:31
+ * @LastEditTime: 2019-11-15 09:29:28
  -->
 <template>
   <div id="input-demo-length">
     <z-input type="text"
           placeholder="请输入内容"
-          :max="5"
           :min="2" />
   </div>
 </template>
