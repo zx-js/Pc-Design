178a3075ca138cfc4da72b600ca211854251fd7a
langxue <641078926@qq.com>
2019-11-15 15:35:53 +0800

    input组件 - 文本框尺寸大小示例

--- docs/.vuepress/components/Input/demo-size.vue
+++ docs/.vuepress/components/Input/demo-size.vue
@@ -4,7 +4,7 @@
  * @Author: 
  * @Date: 2019-11-14 16:03:05
  * @LastEditors: langxue
- * @LastEditTime: 2019-11-14 17:02:00
+ * @LastEditTime: 2019-11-15 11:03:43
  -->
 <template>
   <div id="input-demo-size">
@@ -12,6 +12,7 @@
       <strong>大尺寸：</strong>
       <z-input type="text"
         placeholder="请输入内容"
+        suffixIcon="zxclose"
         class="item-input" />
     </div>
     <br />
@@ -19,15 +20,17 @@
       <strong>中尺寸：</strong>
       <z-input type="text"
         size="small"
+        suffixIcon="zxclose"
         placeholder="请输入内容"
         class="item-input" />
     </div>
     <br />
     <div class="item">
-      <strong>中尺寸：</strong>
+      <strong>小尺寸：</strong>
       <z-input type="text"
         size="mini"
         placeholder="请输入内容"
+        suffixIcon="zxclose"
         class="item-input" />
     </div>
   </div>
@@ -41,7 +44,7 @@
     align-items center;
     margin 0 20px 0 0;
     strong {
-      fobnt-size 14px;
+      font-size 14px;
       color #333;
       width 80px;
     }
