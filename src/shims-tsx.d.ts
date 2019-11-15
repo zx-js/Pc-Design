3e240c2411eaff4c86e704fb823cf6fe73734b55
wangxiaolong <xiaolong1498@gmail.com>
2019-11-15 16:50:52 +0800

    增加vue属性

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -3,19 +3,25 @@
  * @version:
  * @Author: bhabgs
  * @Date: 2019-11-05 16:22:07
- * @LastEditors: bhabgs
- * @LastEditTime: 2019-11-05 16:24:29
+ * @LastEditors: 王晓龙
+ * @LastEditTime: 2019-11-15 15:55:51
  */
 import Vue, { VNode } from 'vue';
 
 declare global {
-    namespace JSX {
-        // tslint:disable no-empty-interface
-        interface Element extends VNode {}
-        // tslint:disable no-empty-interface
-        interface ElementClass extends Vue {}
-        interface IntrinsicElements {
-            [elem: string]: any;
-        }
+  namespace JSX {
+    // tslint:disable no-empty-interface
+    interface Element extends VNode {}
+    // tslint:disable no-empty-interface
+    interface ElementClass extends Vue {}
+    interface IntrinsicElements {
+      [elem: string]: any;
     }
+  }
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    [propName: string]: any;
+  }
 }
