9308f9252fdd4e2faca4cf9c4a0fd7b0cadc5c71
wangxiaolong <xiaolong1498@gmail.com>
2019-11-18 10:55:21 +0800

    nothing

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@
  * @version:
  * @Author: bhabgs
  * @Date: 2019-10-29 10:54:09
- * @LastEditors: bhabgs
- * @LastEditTime: 2019-11-14 09:23:18
+ * @LastEditors: 王晓龙
+ * @LastEditTime: 2019-11-18 10:51:40
  */
 import Vue from 'vue';
 import { compontents } from './components';
@@ -14,19 +14,20 @@ import './styles/index.less';
 
 // 开发组件时可以使用自定义指令
 Vue.use(directives);
+
 // 注册install
 const install = function(Vue: any) {
-    // 指令
-    Vue.use(directives);
-    // 工具
-    Vue.prototype.$Zutil = Zutil;
-    // 组件
-    compontents.forEach((component) => Vue.component(component.name, component.install));
+  // 指令
+  Vue.use(directives);
+  // 工具
+  Vue.prototype.$Zutil = Zutil;
+  // 组件
+  compontents.forEach((component) => Vue.component(component.name, component.install));
 };
 
 // 自动注册组件
 if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
+  install(window.Vue);
 }
 
 export default { install, Zutil };
