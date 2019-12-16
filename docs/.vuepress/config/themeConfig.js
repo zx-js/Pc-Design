aa12b8869a9cfa3d737eb52694faae126db4525b
wangxiaolong <xiaolong1498@gmail.com>
2019-12-16 09:15:07 +0800

    解决冲突

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -3,8 +3,13 @@
  * @version:
  * @Author: bhabgs
  * @Date: 2019-11-07 09:48:14
- * @LastEditors: bhabgs
- * @LastEditTime: 2019-12-13 17:14:58
+<<<<<<< Updated upstream
+ * @LastEditors: 王晓龙
+ * @LastEditTime: 2019-12-16 09:14:27
+=======
+ * @LastEditors: 王晓龙
+ * @LastEditTime: 2019-12-16 09:01:47
+>>>>>>> Stashed changes
  */
 const themeConfig = {
   nav: [
@@ -35,7 +40,12 @@ const themeConfig = {
       ["Message/", "Message 提示"],
       ["Alert/", "Alert 警告"],
       ["Icon/", "Icon 图标"],
-      ["Select/", "Select 选择器"]
+      ['Select/', 'Select 选择器'],
+      ["Tag/", "Tag 标签"],
+      ["timeline/", "Timeline 时间轴"]
+      // ["Drag/", "Drag 拖拽"],
+      // ["Scale/", "Scale 缩放"],
+      // ["ml/", "马良docs"],
     ]
   },
   sidebarDepth: 2
