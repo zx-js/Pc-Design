b2a5ed2a71551b648c9de28cdcf1a5f6ce0f768f
langxue <641078926@qq.com>
2020-01-19 09:32:13 +0800

    msgcode 代码格式化

--- src/components/Input/main.ts
+++ src/components/Input/main.ts
@@ -7,20 +7,19 @@
  * @LastEditTime: 2019-11-14 10:34:09
  */
 
-import Input from './index';
+import Input from "./index";
 
-
-import utils from '../../packages/utils';
+import utils from "../../packages/utils";
 const Msg = new utils.MsgCode();
 const msg = Msg.getMsg({
-    code: ['WIN002', 'WIN001', {}],
-    message: ['错了错了错了', 'aaaaaaa','{}'],
+  code: ["WIN002", "WIN001", {}],
+  message: ["错了错了错了", "aaaaaaa", "{}"],
 });
-console.log('错误提示---', msg);
+console.log("错误提示---", msg);
 const ZInput = {
-    version: '0.0.1',
-    name: 'ZInput',
-    install: Input
+  version: "0.0.1",
+  name: "ZInput",
+  install: Input,
 };
 
 export { ZInput };
--- src/packages/utils/MsgCode/index.ts
+++ src/packages/utils/MsgCode/index.ts
@@ -1,67 +1,65 @@
-import library from './library';
-import $util from '../index';
+import library from "./library";
+import $util from "../index";
 
- export default class MsgCode {
-
+export default class MsgCode {
   private getLib = (code: any) => {
     let msg: string;
     for (const key in library) {
       msg = code + library[code];
     }
     return msg;
-  }
+  };
 
- /**
-  * 用户自定义传入error code和 error mesage
-  */
- private insert = ({ code , message }): string => {
-  let msg: string = '';
-  const codeType = $util.dataType(code);
-  const messageType = $util.dataType(message);
-  if (codeType === 'string' && messageType === 'string') {
-    msg = code + message;
-  } else if (codeType === 'array') {
-    for(let i = 0; i < code.length; i++) {
-      if ($util.dataType(code[i]) === 'string' && $util.dataType(message[i]) === 'string') {
-        const ss = code[i] + ',' + message[i] + ';';
-        msg += ss;
-      } else {
-        console.warn(`${code[i]}此类型无法使用`);
-        continue;
+  /**
+   * 用户自定义传入error code和 error mesage
+   */
+  private insert = ({ code, message }): string => {
+    let msg: string = "";
+    const codeType = $util.dataType(code);
+    const messageType = $util.dataType(message);
+    if (codeType === "string" && messageType === "string") {
+      msg = code + message;
+    } else if (codeType === "array") {
+      for (let i = 0; i < code.length; i++) {
+        if ($util.dataType(code[i]) === "string" && $util.dataType(message[i]) === "string") {
+          const ss = code[i] + "," + message[i] + ";";
+          msg += ss;
+        } else {
+          console.warn(`${code[i]}此类型无法使用`);
+          continue;
+        }
       }
+    } else {
+      console.warn(`${code}或${message}此类型无法使用`);
     }
-  } else {
-    console.warn(`${code}或${message}此类型无法使用`);
-  }
-  return msg;
-}
+    return msg;
+  };
 
   /**
    * @param
    *  code 通过code码查找对应的message
-  */
+   */
   public getMsg = (code: any): string => {
+    console.log(code);
     let msg: string;
     const type = $util.dataType(code);
-    if (type === 'string') {
+    if (type === "string") {
       msg = this.getLib(code);
-      return msg;
-    } else if (type === 'array') {
-      for(let i = 0; i < code.length; i++) {
+    } else if (type === "array") {
+      for (let i = 0; i < code.length; i++) {
         const item = code[i];
-        if ($util.dataType(item) === 'string') {
+        if ($util.dataType(item) === "string") {
           msg = this.getLib(item);
-          return msg;
         } else {
           console.warn(`${item} 此类型无法使用`);
           continue;
         }
       }
-    } else if (type === 'object') {
+    } else if (type === "object") {
       msg = this.insert(code);
-      return msg;
     } else {
       console.warn(`${code} 此类型无法使用`);
     }
-  }
- }
+    return msg;
+  };
+}
